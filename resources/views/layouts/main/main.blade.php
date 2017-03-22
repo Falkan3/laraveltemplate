6444b56bbb2d6ea0fa00d9cc36990c4633e839d7
Adam Kocić <adam.kocic@automatkredytowy.pl>
2017-03-22 17:35:47 +0100

    adding cookie reminder

--- resources/views/layouts/main/main.blade.php
+++ resources/views/layouts/main/main.blade.php
@@ -61,5 +61,24 @@
 <script src="{{ URL::asset('js/sendform.js', env('HTTPS')) }}"></script>
 @yield('custom_js')
 
+<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css" />
+<script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js"></script>
+<script>
+    window.addEventListener("load", function(){
+        window.cookieconsent.initialise({
+            "palette": {
+                "popup": {
+                    "background": "#252e39"
+                },
+                "button": {
+                    "background": "transparent",
+                    "text": "#14a7d0",
+                    "border": "#14a7d0"
+                }
+            },
+            "position": "bottom-left"
+        })});
+</script>
+
 </body>
 </html>
