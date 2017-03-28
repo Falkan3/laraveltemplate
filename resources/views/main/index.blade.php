5bc5fe9748f7f3df0a9792651817ea05c4c62141
Adam Kocić <adam.kocic@automatkredytowy.pl>
2017-03-28 12:01:13 +0200

    carousel

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -22,38 +22,18 @@
     <section class="carousel">
         <div class="container-fluid">
             <div class="row">
-                <div id="myCarousel" class="carousel slide" data-ride="carousel">
-                    <!-- Indicators -->
-                    <ol class="carousel-indicators">
-                        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                        <li data-target="#myCarousel" data-slide-to="1"></li>
-                        <li data-target="#myCarousel" data-slide-to="2"></li>
-                    </ol>
-
-                    <!-- Wrapper for slides -->
-                    <div class="carousel-inner bged" role="listbox">
-                        <div class="item active">
-                            <img src="{{asset('images/banners/bg3.jpg', env('HTTPS'))}}" alt="Chania">
-                        </div>
-
-                        <div class="item">
-                            <img src="{{asset('images/banners/bg3.jpg', env('HTTPS'))}}" alt="Chania">
-                        </div>
-
-                        <div class="item">
-                            <img src="{{asset('images/banners/bg3.jpg', env('HTTPS'))}}" alt="Flower">
-                        </div>
+                <div class="carousel">
+                    <div class="item slide slide1 bged">
+                        <div class="slide_caption"><p>Test 1</p></div>
                     </div>
 
-                    <!-- Left and right controls -->
-                    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-                        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-                        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                    </a>
+                    <div class="item slide slide2 bged">
+                        <div class="slide_caption"><p>Test 2</p></div>
+                    </div>
+
+                    <div class="item slide slide3 bged">
+                        <div class="slide_caption"><p>Test 3</p></div>
+                    </div>
                 </div>
             </div>
         </div>
